604f5d671ddc0945e656c7b2957645025ed31e65
liros-design <128178457+liros-design@users.noreply.github.com>
2025-09-22 12:02:57 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+// GSAP Animations //
+gsap.from(".title", {
+  x: -60,
+  opacity: 0,
+  duration: 4,
+  ease: "power2.out",
+  delay: 0.2,
+  repeat: -1,
+  repeatDelay: 2
+});
+
+
+gsap.from(".dimedis-logo img", {
+  y: 40,
+  opacity: 0,
+  duration: 4,
+  delay: 0.2,
+  ease: "power2.out",
+  repeat: -1,
+  repeatDelay: 2
+});
