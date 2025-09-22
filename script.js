e0b16cd01506166ce4ef14d87c30d855557dd193
liros-design <128178457+liros-design@users.noreply.github.com>
2025-09-22 15:04:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,14 @@ gsap.from(".title", {
   repeat: -1,
   repeatDelay: 2
 });
+
+
+gsap.from(".text", {
+  y: 40,
+  opacity: 0,
+  duration: 4,
+  delay: 0.2,
+  ease: "power2.out",
+  repeat: -1,
+  repeatDelay: 2
+});
