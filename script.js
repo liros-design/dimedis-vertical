b25a2368f20df217e28c6f31bab58d74aa187d3e
liros-design <128178457+liros-design@users.noreply.github.com>
2025-09-22 14:13:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,14 +8,3 @@ gsap.from(".title", {
   repeat: -1,
   repeatDelay: 2
 });
-
-
-gsap.from(".dimedis-logo img", {
-  y: 40,
-  opacity: 0,
-  duration: 4,
-  delay: 0.2,
-  ease: "power2.out",
-  repeat: -1,
-  repeatDelay: 2
-});
