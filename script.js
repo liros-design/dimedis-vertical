f49e0cef3a54b70024edd54b1231ebd5fd8db629
liros-design <128178457+liros-design@users.noreply.github.com>
2025-10-17 11:45:25 +0200

    Refactor gsap animation for question text

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-gsap.to(".question-text", {
-  x: 32,
-  opacity: 1,
-  duration: 2,
-  delay: 0,
-  ease: "power3.out",
-  repeat: -1,
-  repeatDelay: 24
-});
+gsap.fromTo(".question-text",
+  { x: -32, opacity: 0 }, // start off left
+  {
+    x: 0,
+    opacity: 1,
+    duration: 2,
+    delay: 0,
+    ease: "power3.out",
+    repeat: -1,       // loop forever
+    repeatDelay: 24,  // wait 24s before next start
+    onRepeat: function () {
+      // reset instantly to starting state before repeating
+      gsap.set(".question-text", { x: -32, opacity: 0 });
+    }
+  }
+);
+
+
+
+
+
+
 
 gsap.to(".answer-1", {
   x: 32,
