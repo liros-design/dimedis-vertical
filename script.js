ab00f033f710c21d1b191a990ad2f29e587be630
liros-design <128178457+liros-design@users.noreply.github.com>
2025-10-17 12:52:05 +0200

    Add loader animation using GSAP

--- script.js
+++ script.js
@@ -70,3 +70,16 @@ gsap.fromTo(".answer-3",
     }
   }
 );
+
+
+gsap.fromTo(".loader",
+  { scaleX: 1, transformOrigin: "left center" }, // start full width
+  {
+    scaleX: 0,                  // shrink to zero
+    duration: 24,               // takes 24 seconds
+    delay: 3, 
+    ease: "linear",             // smooth, steady reduction
+    repeat: -1,                 // infinite loop
+    repeatDelay: 0              // restart immediately
+  }
+);
