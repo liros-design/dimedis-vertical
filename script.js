0677a06a1341de9c769bd288b7d8ca61606449b5
liros-design <128178457+liros-design@users.noreply.github.com>
2025-10-17 10:21:23 +0200

    Update GSAP animations with new delays and targets

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-// GSAP Animations //
-gsap.from(".title", {
-  x: -60,
-  opacity: 0,
-  duration: 4,
-  ease: "power2.out",
-  delay: 0.2,
+gsap.to(".question-text", {
+  x: 32,
+  opacity: 1,
+  duration: 2,
+  delay: 0,
+  ease: "power3.out",
   repeat: -1,
-  repeatDelay: 2
+  repeatDelay: 24
 });
 
-
-gsap.from(".text", {
-  y: 40,
-  opacity: 0,
-  duration: 4,
-  delay: 0.2,
-  ease: "power2.out",
+gsap.to(".answer-1", {
+  x: 32,
+  opacity: 1,
+  duration: 2,
+  delay: 3,
+  ease: "power3.out",
   repeat: -1,
-  repeatDelay: 2
+  repeatDelay: 24
+});
+
+gsap.to(".answer-2", {
+  x: 32,
+  opacity: 1,
+  duration: 2,
+  delay: 4,
+  ease: "power3.out",
+  repeat: -1,
+  repeatDelay: 24
+});
+
+gsap.to(".answer-3", {
+  x: 32,
+  opacity: 1,
+  duration: 2,
+  delay: 5,
+  ease: "power3.out",
+  repeat: -1,
+  repeatDelay: 24
 });
