60cfc3c0478b30f53fce00dfc74e7b34c67960ae
liros-design <128178457+liros-design@users.noreply.github.com>
2025-10-17 13:05:55 +0200

    Adjust loader animation duration and delays

--- script.js
+++ script.js
@@ -76,10 +76,10 @@ gsap.fromTo(".loader",
   { scaleX: 1, transformOrigin: "left center" }, // start full width
   {
     scaleX: 0,                  // shrink to zero
-    duration: 24,               // takes 24 seconds
+    duration: 20,               // takes 24 seconds
     delay: 4, 
-    ease: "linear",             // smooth, steady reduction
+    ease: "linear",             // smooth
     repeat: -1,                 // infinite loop
-    repeatDelay: 0              // restart immediately
+    repeatDelay: 4              // restart
   }
 );
