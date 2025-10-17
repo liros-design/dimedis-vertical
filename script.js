4f29c5b3acaf2f423ef7d1bdddd80ecbf8c1cf11
liros-design <128178457+liros-design@users.noreply.github.com>
2025-10-17 13:15:22 +0200

    Refactor GSAP animations for timing adjustments

--- script.js
+++ script.js
@@ -1,85 +1,72 @@
+// === QUESTION ===
 gsap.fromTo(".question-text",
-  { x: -32, opacity: 0 }, // start off left
+  { x: -32, opacity: 0 },
   {
     x: 0,
     opacity: 1,
     duration: 2,
     delay: 0,
     ease: "power3.out",
-    repeat: -1,       // loop forever
-    repeatDelay: 30,  // wait 30s before next start
-    onRepeat: function () {
-      // reset instantly to starting state before repeating
-      gsap.set(".question-text", { x: -32, opacity: 0 });
-    }
+    repeat: -1,
+    repeatDelay: 28, // 30s total - 2s animation = 28s
+    onRepeat: () => gsap.set(".question-text", { x: -32, opacity: 0 })
   }
 );
 
-
-
+// === ANSWER 1 ===
 gsap.fromTo(".answer-1",
-  { x: -32, opacity: 0 }, // start off left
+  { x: -32, opacity: 0 },
   {
     x: 0,
     opacity: 1,
     duration: 2,
     delay: 1,
     ease: "power3.out",
-    repeat: -1,       // loop forever
-    repeatDelay: 30,  // wait 30s before next start
-    onRepeat: function () {
-      // reset instantly to starting state before repeating
-      gsap.set(".answer-1", { x: -32, opacity: 0 });
-    }
+    repeat: -1,
+    repeatDelay: 27, // 30 - (1 + 2)
+    onRepeat: () => gsap.set(".answer-1", { x: -32, opacity: 0 })
   }
 );
 
-
+// === ANSWER 2 ===
 gsap.fromTo(".answer-2",
-  { x: -32, opacity: 0 }, // start off left
+  { x: -32, opacity: 0 },
   {
     x: 0,
     opacity: 1,
     duration: 2,
     delay: 2,
     ease: "power3.out",
-    repeat: -1,       // loop forever
-    repeatDelay: 30,  // wait 30s before next start
-    onRepeat: function () {
-      // reset instantly to starting state before repeating
-      gsap.set(".answer-2", { x: -32, opacity: 0 });
-    }
+    repeat: -1,
+    repeatDelay: 26,
+    onRepeat: () => gsap.set(".answer-2", { x: -32, opacity: 0 })
   }
 );
 
-
-
+// === ANSWER 3 ===
 gsap.fromTo(".answer-3",
-  { x: -32, opacity: 0 }, // start off left
+  { x: -32, opacity: 0 },
   {
     x: 0,
     opacity: 1,
     duration: 2,
     delay: 3,
     ease: "power3.out",
-    repeat: -1,       // loop forever
-    repeatDelay: 30,  // wait 30s before next start
-    onRepeat: function () {
-      // reset instantly to starting state before repeating
-      gsap.set(".answer-3", { x: -32, opacity: 0 });
-    }
+    repeat: -1,
+    repeatDelay: 25,
+    onRepeat: () => gsap.set(".answer-3", { x: -32, opacity: 0 })
   }
 );
 
-
+// === LOADER ===
 gsap.fromTo(".loader",
-  { scaleX: 1, transformOrigin: "left center" }, // start full width
+  { scaleX: 1, transformOrigin: "left center" },
   {
-    scaleX: 0,                  // shrink to zero
-    duration: 20,               // takes 24 seconds
-    delay: 5, 
-    ease: "linear",             // smooth
-    repeat: -1,                 // infinite loop
-    repeatDelay: 30              // restart
+    scaleX: 0,
+    duration: 24,  // shrink over 24s
+    delay: 5,      // starts after answers finish
+    ease: "linear",
+    repeat: -1,
+    repeatDelay: 1 // 30s total cycle: 5s before + 24s shrink + 1s buffer
   }
 );
